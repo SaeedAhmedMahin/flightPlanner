d91596aece486b7371b7ad3b6b390063faa6c6b0
Saeed Ahmed Mahin <saeedahmedmahin@gmail.com>
2026-08-29 17:50:31 +0600

    chore: add searchState

---
+++ include/Algorithms/seachState.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <cstdint>
+
+struct SearchState {
+  uint32_t node_id;
+  uint16_t total_cost;   // The feature we are minimizing (Price or Duration)
+  uint16_t current_time; // Absolute time elapsed + initial departure (for
+                         // layover logic)
+
+  // C++ std::priority_queue is a Max-Heap by default.
+  // Overloading '>' flips it into a Min-Heap so the lowest total_cost pops
+  // first.
+  bool operator>(const SearchState &other) const {
+    return total_cost > other.total_cost;
+  }
+};
